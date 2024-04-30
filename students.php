69f17dac9b3d3e82c3d187d0a24d21058b636357
adz3k <124911028+adz3k@users.noreply.github.com>
2024-04-30 10:43:09 +0100

    i made changes to the headings for the students tables

--- students.php
+++ students.php
@@ -1,4 +1,4 @@
-
+<?php
 include("_includes/config.inc");
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
@@ -13,36 +13,32 @@ include("_includes/config.inc");
       // Build SQL statment that selects a student's modules
       // TO DO REWRITE QUERY TO STUDENT SELECT
      // $sql = "select * from studentmodules sm, module m where m.modulecode = sm.modulecode and sm.studentid = '" . $_SESSION['id'] ."';";
+      
+     $sql = "select * from student;";
 
       $result = mysqli_query($conn,$sql);
 
       // prepare page content
       $data['content'] .= "<table border='1'>";
-      $data['content'] .= "<tr><th colspan='5' align='center'>firstname</th></tr>";
-      $data['content'] .= "<tr><th>secondname</th><th>Type</th><th>Level</th></tr>";
-      th><th>Level</th></tr>"th><th>Level</th></tr>"th><th>Level</th></tr>"th><th>Level</th></tr>"th><th>Level</th></tr>";
+      $data['content'] .= "<tr><th>Student Id </th><th>DOB</th><th>firstname</th></th><th>lastname</th><th>DOB<
+      </th><th>DOB<</th><th>DOB<</th><th>DOB<</tr>";
+      
       // Display the modules within the html table
       while($row = mysqli_fetch_array($result)) {
          $data['content'] .= "<tr>";
-         $data['content'] .= "<td>      {$row[modulecode]  $row["firstname"]} </td>";
-         $data['content'] .= "<td>      {$row[modulecode]  $row["secondname"]} </td>"
-         $data['content'] .= "<td>      {$row[modulecode]  $row["level1"]} </td>";
-         $data['content'] .= "<td>      {$row[modulecode]  $row["level1"]} </td>";
-         $data['content'] .= "<td>      {$row[modulecode]  $row["level1"} </td>";
-         $data['content'] .= "<td>      {$row[modulecode]  $row["level1"]} </td>";
-         $data['content'] .= "<td>      {$row[modulecode]  $row["level1"} </td>";
-         $data['content'] .= "<td>      {$row[modulecode]  $row["level1"]} </td>";
-
-            
-         
-         $row[level] </td></tr>";
+         $data['content'] .= "<td>       {$row["studentid"]} </td>";
+         $data['content'] .= "<td>       {$row["dob"]} </td>";
+         $data['content'] .= "<td>       {$row["firstname"]} </td>";
+         $data['content'] .= "<td>       {$row["lastname"]} </td>";
+          $data['content'] .="</td></tr>";
       }
+    
       $data['content'] .= "</table>";
 
       // render the template
       echo template("templates/default.php", $data);
 
-   } else {
+     } else {
       header("Location: index.php");
    }
 
